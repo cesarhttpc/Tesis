10e3d8af57caa23f8ffd5e1066438d6e0fe429ac
cesarhttpc <ce_rarin@hotmail.com>
2024-02-05 20:07:54 -0600

    MCMC g y b

--- Semana1/Semana1.py
+++ Semana1/Semana1.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+from scipy.stats import uniform, norm
 
 
 # %%
@@ -52,10 +53,54 @@ def logposterior(g, b, t, y_obs, t_obs, sigma = 1):
 
     return Logf_post
 
+def MetropolisHastingsRW(tamañoMuestra = 50000, propuesta = 'Normal'):
 
-# Parametetros
-g = 10
-b = .7
+    # Punto inicial (parametros)
+    x = np.array([10,.7])
+
+    Muestra1 = np.zeros(tamañoMuestra)  # g
+    Muestra2 = np.zeros(tamañoMuestra)  # b
+    Muestra1[0] = x[0]
+    Muestra2[0] = x[1] 
+
+
+    for k in range(tamañoMuestra-1):
+
+        # Simulación de propuesta
+        if propuesta == 'Normal':
+
+            sigma1 = 0.5
+            sigma2 = 0.05
+            e1 = norm.rvs(0,sigma1)
+            e2 = norm.rvs(0,sigma2)
+            e = np.array([e1,e2])
+            y = x + e 
+
+        if propuesta == 'Uniforme':
+            epsilon1 = 0.1
+            epsilon2 = 2
+            e1 = (1-2*uniform.rvs(0,1))*epsilon1
+            e2 = (1-2*uniform.rvs(0,1))*epsilon2
+
+            e = np.array([e1,e2])
+            y = x + e
+
+        # Cadena de Markov
+        cociente = np.exp(logposterior(y[0],y[1],t,y_obs,t_obs)- logposterior(x[0],x[1],t,y_obs,t_obs))
+        # cociente = posterior(y[0], y[1], x_i)/posterior(x[0], x[1], x_i)
+        # p_min = min(1,cociente)
+
+        # Transición de la cadena
+        if uniform.rvs(0,1) < cociente :    #Ensayo Bernoulli
+            Muestra1[k+1] = y[0]
+            Muestra2[k+1] = y[1]
+            x = y
+        else:
+            Muestra1[k+1] = x[0]
+            Muestra2[k+1] = x[1]
+        
+    return Muestra1,Muestra2
+
 
 # Condiciones iniciales
 y0 = [0, 0.0]  
@@ -70,26 +115,29 @@ y_obs = np.array([0, 0.06 ,0.16, 0.26, 0.36, 0.46, 0.56])
 t_obs = np.array([0, 0.071, 0.158, 0.220, 0.267, 0.306, 0.340])
 
 
+# print(logposterior(g,b,t,y_obs, t_obs,))
 
+Posterior_g, Posterior_b = MetropolisHastingsRW()
 
+# %%
 
+plt.hist(Posterior_g, bins = 40)
+plt.show()
 
+plt.hist(Posterior_b, bins = 40)
+plt.show()
 
-
-
-print(logposterior(g,b,t,y_obs, t_obs,))
-
-
-
-
-
-
+print(np.mean(Posterior_g))
+print(np.mean(Posterior_b))
 
 
 # %%
 
 # /////////// Visualización ////////
 
+# Parametetros
+g = 10
+b = .7
 
 # Soluciones de la ecuación dínamica
 solutions = odeint(damping, y0, t, args=(g,b))
@@ -117,7 +165,7 @@ plt.legend()
 # z = lambda x: 9.7*x**2/2
 # plt.plot(t,z(t))
  
-plt.plot( t_o, y_o, 'o')
+plt.plot( t_obs, y_obs, 'o')
 plt.show()
 
 plt.plot(x,v)
